a9a92e5d3c2f38476aa364e494d214f8393c1b37
mateuszrosa <mateusz.rosa@gmail.com>
2020-06-11 19:07:40 +0200

    click event on imgages

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("object");
+const products = document.querySelectorAll(".product");
+
+products.forEach((product) => {
+  product.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  });
+});
